9b414e4ee0ca62eb23b2dd973d7c561c12dfac97
Sönke Hahn <soenkehahn@gmail.com>
2023-09-11 17:54:35 -0400

    Square waves (#4)
    Co-authored-by: Alex David <git@alexd.me>

--- sine.nix
+++ sine.nix
@@ -14,16 +14,26 @@
   };
 
   sine =
-    frequency: duration:
+    frequency: duration: vol:
     let seconds = duration / 6; in
     pkgs.runCommand
       "sine-${builtins.toString frequency}-${builtins.toString seconds}.wav"
       {
-        nativeBuildInputs = [ pkgs.sox ];
+        nativeBuildInputs = [ pkgs.sox pkgs.calc ];
       }
       ''
-        sox -V -r 48000 -n -b 16 -c 2 $out synth ${builtins.toString seconds} sin ${builtins.toString frequency} vol -10dB
+        sox -V -r 48000 -n -b 16 -c 2 $out synth ${builtins.toString seconds} sin ${builtins.toString frequency} vol $(calc 'floor(log(${builtins.toString vol}+1)*12-25)')dB
       '';
+
+  square = f: d: vol: overlay [
+    (sine f d vol)
+    (sine (f*3.0) d (vol / 3.0))
+    (sine (f*5.0) d (vol / 5.0))
+    (sine (f*7.0) d (vol / 7.0))
+    (sine (f*9.0) d (vol / 9.0))
+    (sine (f*11.0) d (vol / 11.0))
+    (sine (f*13.0) d (vol / 13.0))
+  ];
 
   binarySeq = first: second:
     pkgs.runCommand "sequence.wav"
@@ -34,7 +44,7 @@
         sox ${first} ${second} $out
       '';
 
-  sequence = lib.fold binarySeq (sine 14000 0.1);
+  sequence = lib.fold binarySeq (sine 14000 0.1 0.0);
 
   binaryOverlay = first: second:
     pkgs.runCommand "overlay.wav"
@@ -45,5 +55,5 @@
         ffmpeg -i ${first} -i ${second} -filter_complex amix=inputs=2:duration=longest $out
       '';
 
-  overlay = lib.fold binaryOverlay (sine 14000 0.1);
+  overlay = lib.fold binaryOverlay (sine 14000 0.1 0.0);
 }
