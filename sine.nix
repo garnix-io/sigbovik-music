7131c8fda32c8c7e95c963df90775303f04187d6
lucasew <lucas59356@gmail.com>
2024-04-19 17:16:08 -0300

    small refactor, doing calculations directly in nix instead of relying on IFD
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- sine.nix
+++ sine.nix
@@ -19,20 +19,16 @@
     aMinor = chord [ a2 c4 e4 ];
   };
 
-  fromMidi = midi: 440 * (power (power 2.0 (1.0 / 12.0)) (midi - 69.0));
+  ipower = x: y:
+    if y == 0 then
+      1.0
+    else if y < 0 then
+      1.0 / (ipower x (y * -1))
+    else
+      1.0 * x * (ipower x (y - 1));
 
-  power = a: b:
-    let
-      result = pkgs.runCommand
-        "power of ${builtins.toString a} and ${builtins.toString b}"
-        {
-          nativeBuildInputs = [ pkgs.python3 ];
-        }
-        ''
-          python3 -c "print(pow(${builtins.toString a}, ${builtins.toString b}))" > $out
-        '';
-    in
-    import result;
+  midiConstant = 1.0594630943592953; # 2.0**(1.0/12.0)
+  fromMidi = midi: 440 * (ipower midiConstant (midi - 69));
 
   sine =
     frequency: duration: vol:
@@ -40,7 +36,7 @@
     pkgs.runCommand
       "sine-${builtins.toString frequency}-${builtins.toString seconds}.wav"
       {
-        nativeBuildInputs = [ pkgs.sox pkgs.calc ];
+        nativeBuildInputs = [ pkgs.sox ];
       }
       ''
         sox -V -r 48000 -n -b 16 -c 2 $out synth ${builtins.toString seconds} sin ${builtins.toString frequency} vol ${builtins.toString (vol / 100)}
@@ -50,18 +46,18 @@
 
   square = f: d: vol: overlay [
     (sine f d vol)
-    (sine (f*3.0) d (vol / 3.0))
-    (sine (f*5.0) d (vol / 5.0))
-    (sine (f*7.0) d (vol / 7.0))
-    (sine (f*9.0) d (vol / 9.0))
-    (sine (f*11.0) d (vol / 11.0))
-    (sine (f*13.0) d (vol / 13.0))
+    (sine (f * 3.0) d (vol / 3.0))
+    (sine (f * 5.0) d (vol / 5.0))
+    (sine (f * 7.0) d (vol / 7.0))
+    (sine (f * 9.0) d (vol / 9.0))
+    (sine (f * 11.0) d (vol / 11.0))
+    (sine (f * 13.0) d (vol / 13.0))
   ];
 
   chord = notes: duration: volume:
     overlay (pkgs.lib.lists.map
-        (frequency: sine frequency duration volume)
-        notes);
+      (frequency: sine frequency duration volume)
+      notes);
 
   binarySeq = first: second:
     let sum = getCount first + getCount second;
@@ -69,9 +65,12 @@
     (pkgs.runCommand "sequence-of-${builtins.toString sum}-notes.wav"
       {
         nativeBuildInputs = [ pkgs.sox ];
+        env = {
+          inherit first second;
+        };
       }
       ''
-        sox ${first} ${second} $out
+        sox $first $second $out
       '') // { count = sum; };
 
   sequence = lib.fold binarySeq (sine 14000 0.1 0.0);
@@ -82,9 +81,12 @@
     (pkgs.runCommand "overlay-of-${builtins.toString sum}-notes.wav"
       {
         nativeBuildInputs = [ pkgs.ffmpeg ];
+        env = {
+          inherit first second;
+        };
       }
       ''
-        ffmpeg -i ${first} -i ${second} -filter_complex amix=inputs=2:duration=longest $out
+        ffmpeg -i $first -i $second -filter_complex amix=inputs=2:duration=longest $out
       '') // { count = sum; };
 
   overlay = lib.fold binaryOverlay (sine 14000 0.1 0.0);
